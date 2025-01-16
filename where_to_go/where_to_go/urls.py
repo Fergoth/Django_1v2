2c5a34e9171a6e51ba1471205ac80382279cd815
fergoth <fergothew@gmail.com>
2025-01-16 17:09:41 +0300

    Add json api with place info

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_main)
+    path('', views.show_main),
+    path('place/<int:place_id>', views.place_details)
 ]
 if settings.DEBUG:
     urlpatterns += static(
--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from places.models import Place
+
 
 def show_main(request):
 
@@ -13,7 +15,7 @@ def show_main(request):
                     "type": "Point",
                     "coordinates": [place.longitude, place.latitude]
                 },
-                "properties": 
+                "properties":
                 {
                     "title": place.title,
                     "placeId": place.id,
@@ -24,3 +26,25 @@ def show_main(request):
         ]
     }
     return render(request, 'index.html', context={'places': places})
+
+
+def place_details(request, place_id):
+    place_queryset = Place.objects.prefetch_related('images')
+    place = get_object_or_404(place_queryset, id=place_id)
+    serialized_place = {
+        'title': place.title,
+        'imgs': [image.image.url for image in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.latitude,
+            'lon': place.longitude
+        }
+    }
+    return JsonResponse(
+        serialized_place,
+        json_dumps_params={
+            'ensure_ascii': False,
+            'indent': 4
+        }
+    )
