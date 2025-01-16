aae4ffa6cb07aa472c89531ec835f6b28c73789d
fergoth <fergothew@gmail.com>
2025-01-16 17:55:41 +0300

    add image prewview for imageinline

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from places.models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
 
+    readonly_fields = ['preview']
+    fields = ('image', 'preview', 'order')
+
+    def preview(self, obj):
+        max_height = 200
+        if obj.image.height >= max_height:
+            height = max_height
+            width = int(obj.image.width * (max_height/obj.image.height))
+        else:
+            height = obj.image.height
+            width = obj.image.width
+        return format_html(
+            '<img src="{url}" width="{width}" height={height} />'.
+            format(
+                url=obj.image.url,
+                width=width,
+                height=height,
+            )
+        )
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
@@ -13,4 +33,13 @@ class PlaceAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ['test']
+
+    def test(self, obj):
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=obj.image.width,
+            height=obj.image.height,
+            )
+    )
+
