780a049d66cc54bf333f8fd71c52f6f43afdbafd
fergoth <fergothew@gmail.com>
2025-01-19 18:40:36 +0300

    updated load_place script with error handling

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -9,7 +9,11 @@ from urllib.parse import urlparse
 def save_image(place, url):
     filepath = urlparse(url).path
     filename = os.path.basename(filepath)
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.HTTPError as err:
+        print(f'Проблемы с загрузкой данных по {url}', err)
+        return
     imagefile = ContentFile(response.content)
     obj = PlaceImage.objects.create(
         place=place
@@ -25,14 +29,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         url = options['url']
-        r = requests.get(url)
-        js = r.json()
-        title = js['title']
-        image_urls = js['imgs']
-        description_short = js['description_short']
-        description_long = js['description_long']
-        lng = js['coordinates']['lng']
-        lat = js['coordinates']['lat']
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print('Проблемы с загрузкой данных по url', err)
+            return
+        try:
+            js = r.json()
+            title = js['title']
+            image_urls = js['imgs']
+            description_short = js['description_short']
+            description_long = js['description_long']
+            lng = js['coordinates']['lng']
+            lat = js['coordinates']['lat']
+        except KeyError as e:
+            print('Некорректный формат json', e)
+            return
+        except requests.exceptions.JSONDecodeError:
+            print('По ссылке находится не json файл')
         place, created = Place.objects.get_or_create(
             title=title,
             description_long=description_long,
@@ -41,5 +56,9 @@ class Command(BaseCommand):
             latitude=lat
         )
         if created:
+            print('Сохранена локация, сохраняем фотографии..')
             for image_url in image_urls:
                 save_image(place, image_url)
+            print('Фотографии сохранены')
+        else:
+            print('Локация уже существует')
