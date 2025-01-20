53dcdae34d7f54a1d238f262037e3b34f929aac7
fergoth <fergothew@gmail.com>
2025-01-20 13:42:12 +0300

    set SECRET_KEY env required

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', True)
 
